714696fc971752516e075b01ccaab20182700541
MECCNR <e.migalckin@yandex.ru>
2022-08-08 11:39:47 +0900

    version

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+        public String name; // поле для имени
+        public String surname;
+        public String patronymic;
+        public String phone;
+        public String passport;
+        public boolean subscription;
+        FormDate birthday = new FormDate();
+}
